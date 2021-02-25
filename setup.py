bd1eda4da180736ea0d01debf7ec0460e0017ef0
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2021-02-25 11:44:31 +0100

    add requests as dependency

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['requests >= 2.20; python_version >= "3.0"'],
     python_requires=">=3.6",
 )
