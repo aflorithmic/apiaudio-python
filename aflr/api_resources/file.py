c8054eeabbee3a0cc4a0c63086b3e2688bb5c373
larsAFl <lars@aflorithmic.ai>
2021-03-02 11:21:50 +0100

    API0006 add get/download mastering file to sk

--- aflr/api_resources/file.py
+++ aflr/api_resources/file.py
@@ -8,6 +8,7 @@ class File(APIRequest):
     def __init__(self):
         super().__init__()
         self.url = self.api_base + "/file"
+        self.file_master = self.url + "/mastering"
 
     def config_test(self):
         return f"Configured to transact {self.OBJECT_NAME} objects to {self.url} with api_key = {self.api_key}"
@@ -18,5 +19,13 @@ class File(APIRequest):
     # get sound files
     # TBD
 
-    # get mastering fiels
-    # TBD
+	# get mastering file
+	def retrieve_master(self, scriptId, parameters={}):
+		parameters.update({'scriptId': scriptId})
+		return self._get_request(url=self.file_master, request_params=parameters)
+
+    # download mastering file
+	def download_master(self, scriptId, parameters={}, destination="."):
+		url = self.retrieve_master(scriptId=scriptId, parameters=parameters).get("url")
+		local_filename = self._download_request(url=url, destination=destination) 
+		return local_filename
