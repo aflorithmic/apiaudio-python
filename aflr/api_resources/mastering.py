2d495038a9bd4e6dc761f2e4bd6d337bb181b0d8
larsAFl <lars@aflorithmic.ai>
2021-03-02 12:06:37 +0100

    changes for review

--- aflr/api_resources/mastering.py
+++ aflr/api_resources/mastering.py
@@ -7,18 +7,18 @@ class Mastering(APIRequest):
 
     def __init__(self):
         super().__init__()
-        self.url = self.api_base + "/file/mastering"
+        self.file_url = self.api_base + "/file/mastering"
 
     def config_test(self):
         return f"Configured to transact {self.OBJECT_NAME} objects to {self.url} with api_key = {self.api_key}"
 
 	# get mastering file
-    def retrieve_master(self, scriptId, parameters={}):
+    def retrieve(self, scriptId, parameters={}):
         parameters.update({'scriptId': scriptId})
-        return self._get_request(url=self.url, request_params=parameters)
+        return self._get_request(url=self.file_url, request_params=parameters)
 
     # download mastering file
-    def download_master(self, scriptId, parameters={}, destination="."):
+    def download(self, scriptId, parameters={}, destination="."):
         url = self.retrieve_master(scriptId=scriptId, parameters=parameters)
         url = url if type(url) == str else url.get('url')
         local_filename = self._download_request(url=url, destination=destination) 
