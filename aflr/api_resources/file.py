8ec5c54049d0e1d473da696436a0a678458b7f26
larsAFl <lars@aflorithmic.ai>
2021-03-02 11:50:21 +0100

    delete useles url

--- aflr/api_resources/file.py
+++ aflr/api_resources/file.py
@@ -8,7 +8,6 @@ class File(APIRequest):
     def __init__(self):
         super().__init__()
         self.url = self.api_base + "/file"
-        self.file_master = self.url + "/mastering"
 
     def config_test(self):
         return f"Configured to transact {self.OBJECT_NAME} objects to {self.url} with api_key = {self.api_key}"
