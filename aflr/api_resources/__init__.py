a146a4d66786200cf558a37a8906f6051187342c
zeritte <zeritte@gmail.com>
2021-03-17 17:57:27 +0300

    export new submodule

--- aflr/api_resources/__init__.py
+++ aflr/api_resources/__init__.py
@@ -2,3 +2,4 @@ from aflr.api_resources.script import Script
 from aflr.api_resources.speech import Speech
 from aflr.api_resources.voice import Voice
 from aflr.api_resources.mastering import Mastering
+from aflr.api_resources.sync_tts import SyncTTS
