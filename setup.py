b9f6997c5873168c50412aa6259725cdb113a1d2
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2021-03-18 15:40:01 +0100

    update version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aflr",
-    version="0.1.3",
+    version="0.2.0",
     author="Antonio Tripiana",
     author_email="antonio@aflorithmic.ai",
     description="Python library for the Aflorithmic api.audio API",
