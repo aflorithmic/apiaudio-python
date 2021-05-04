98b490c1c06852541566f6b157e1b73785650bd1
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2021-05-04 15:57:45 +0200

    add voice list_paramters support@

--- aflr/api_resources/voice.py
+++ aflr/api_resources/voice.py
@@ -8,7 +8,11 @@ class Voice(APIRequest):
     def __init__(self):
         super().__init__()
         self.url = self.api_base + "/voice"
+        self.parameters_url = self.api_base + "/voice/parameter"
 
     # get voices list
     def list(self, **params):
         return self._get_request(url=self.url, request_params=params)
+
+    def list_parameters(self):
+        return self._get_request(url=self.parameters_url)
