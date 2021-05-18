1031f4bb90730224726ab31a67c4f588d03d065c
zeritte <zeritte@gmail.com>
2021-05-18 19:54:39 +0300

    removing noncalled part

--- aflr/api_resources/script.py
+++ aflr/api_resources/script.py
@@ -5,10 +5,5 @@ class Script(ListableResource, CreatableResource, RetrievableResource):
     OBJECT_NAME = "script"
     resource_path = "/script"
 
-    def __init__(self):
-        super().__init__()  # add params to the init performed by the base-class
-        # good read is https://stackoverflow.com/questions/1385759/should-init-call-the-parent-classs-init
-        self.url = self.api_base + "/script"
-
     def config_test(self):
         return f"Configured to transact {self.OBJECT_NAME} objects to {self.url} with api_key = {self.api_key}"
