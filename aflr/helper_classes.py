ea1adbfc0193e85995a6cac46e1e8df47003570d
a96lex <a96lex@gmail.com>
2021-05-18 20:14:45 +0200

    retrieve works for speech too now

--- aflr/helper_classes.py
+++ aflr/helper_classes.py
@@ -24,5 +24,34 @@ class RetrievableResource(APIRequest):
         super().__init__()
 
     @classmethod
-    def retrieve(cls, objectId):
-        return cls._get_request(path_param=objectId)
+    def retrieve(cls, scriptId, section=None, parameters=None):
+        params = parameters or {}
+        params.update({"scriptId": scriptId})
+        if section:
+            params.update({"section": section})
+        if cls.file_url:
+            print(cls.file_url)
+            return cls._get_request(
+                url=cls.file_url,
+                request_params=params,
+            )
+        else:
+            return cls._get_request(path_param=scriptId)
+
+
+class DownloadableResource(APIRequest):
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def download(cls, scriptId, section=None, parameters=None, destination="."):
+        parameters = parameters or {}
+        audio_files = cls.retrieve(scriptId, section, parameters)
+        local_filenames = []
+        print(audio_files)
+        for key, value in audio_files.items():
+            # Review "value"! list of string...
+            local_filename = cls._download_request(url=value, destination=destination)
+            local_filenames.append(local_filename)
+
+        return local_filenames
