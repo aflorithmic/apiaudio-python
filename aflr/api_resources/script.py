4d126825231466beffcfa1449395ee52d80a0caa
a96lex <a96lex@gmail.com>
2021-05-18 19:27:04 +0200

    simplified scrpt respurce

--- aflr/api_resources/script.py
+++ aflr/api_resources/script.py
@@ -1,9 +1,7 @@
 from aflr.helper_classes import CreatableResource, ListableResource, RetrievableResource
+import aflr
 
 
 class Script(ListableResource, CreatableResource, RetrievableResource):
     OBJECT_NAME = "script"
-    resource_path = "/script"
-
-    def config_test(self):
-        return f"Configured to transact {self.OBJECT_NAME} objects to {self.url} with api_key = {self.api_key}"
+    url = f"{aflr.api_base}/{OBJECT_NAME}"
