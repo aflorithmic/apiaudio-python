b235d2324225ab6ba8a374a184ca96c3bd1199e3
a96lex <a96lex@gmail.com>
2021-05-19 18:08:40 +0200

    removed custom method

--- aflr/api_resources/mastering.py
+++ aflr/api_resources/mastering.py
@@ -10,13 +10,3 @@ class Mastering(CreatableResource, RetrievableResource, DownloadableResource):
     OBJECT_NAME = "mastering"
     resource_path = "/mastering"
     file_url = "/file/mastering"
-
-    # download mastering file
-    def download(self, scriptId, parameters={}, destination="."):
-        url = self.retrieve(scriptId=scriptId, parameters=parameters).get("url")
-        if not url:
-            raise Exception(
-                "Error: No mastered files found. Please try again in a few seconds."
-            )
-        local_filename = self._download_request(url=url, destination=destination)
-        return local_filename
