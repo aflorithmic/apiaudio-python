4c42e9f3e780ee593c530763acda9f9126372d6f
a96lex <a96lex@gmail.com>
2021-05-19 18:42:00 +0200

    update sound class

--- aflr/api_resources/sound.py
+++ aflr/api_resources/sound.py
@@ -1,28 +1,15 @@
-import aflr
-from aflr.api_request import APIRequest
+from aflr.helper_classes import (
+    CreatableResource,
+    DownloadableResource,
+    ListableResource,
+    RetrievableResource,
+)
 
 
-class Sound(APIRequest):
+class Sound(
+    CreatableResource, DownloadableResource, RetrievableResource, ListableResource
+):
     OBJECT_NAME = "sound"
-
-    def __init__(self):
-        super().__init__()  # add params to the init performed by the base-class
-        # good read is https://stackoverflow.com/questions/1385759/should-init-call-the-parent-classs-init
-        self.url = self.api_base + "/sound"
-        self.file_url = self.api_base + "/file/sound"
-        self.bg_url = self.api_base + "/file/bg"
-
-    def create(self, **params):
-        return self._post_request(url=self.url, json=params)
-
-    def retrieve(self, scriptId, parameters={}):
-        parameters.update({"scriptId": scriptId})
-        return self._get_request(url=self.file_url, request_params=parameters)
-
-    def list(self):
-        return self._get_request(url=self.bg_url)
-
-    def download(self, scriptId, parameters={}, destination="."):
-        url = self.retrieve(scriptId=scriptId, parameters=parameters).get("url")
-        local_filename = self._download_request(url=url, destination=destination)
-        return local_filename
+    resource_path = "/sound"
+    file_url = "/file/sound"
+    bg_url = "/file/bg"
