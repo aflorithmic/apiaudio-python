7465b6e2dbf8be4acd9df6959470a3941582bc37
a96lex <a96lex@gmail.com>
2021-05-20 11:58:33 +0200

    updated memoization

--- aflr/api_request.py
+++ aflr/api_request.py
@@ -8,7 +8,7 @@ from functools import lru_cache
 
 
 class APIRequest:
-    @lru_cache
+    @lru_cache(maxsize=10, typed=False)
     def _api_key_checker(api_key=None):
         if api_key == None or api_key == "your-key":
             api_key = os.environ.get("aflr_key", None)
