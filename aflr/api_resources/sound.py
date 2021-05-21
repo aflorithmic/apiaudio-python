653b9103141da00bdade527b94f974808a1202b9
a96lex <a96lex@gmail.com>
2021-05-21 15:06:05 +0200

    added custom methods for Sound

--- aflr/api_resources/sound.py
+++ aflr/api_resources/sound.py
@@ -13,3 +13,12 @@ class Sound(
     resource_path = "/sound"
     file_url = "/file/sound"
     bg_url = "/file/bg"
+    soundtemplates_url = "/file/soundtemplates"
+
+    @classmethod
+    def list_sound_templates(cls):
+        return cls._get_request(path_param=cls.soundtemplates_url)
+
+    @classmethod
+    def list(cls):
+        return cls._get_request(path_param=cls.bg_url)
