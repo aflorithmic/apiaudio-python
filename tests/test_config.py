d0d8b202197d805e4f444c4b595a6020ea6f4ca8
springcoil <peadarcoyle@googlemail.com>
2021-05-27 17:12:02 +0100

    Adding in other tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -21,4 +21,22 @@ def test_list_parameters():
 def test_sound_design():
     sounds = Sound().list_sound_templates()
     city_nights=sounds.get("templateFiles").get("citynights")
-    assert city_nights[1].keys() == dict_keys(['citynights__music.wav'])
+    # Still not sure how to get this to work properly
+    assert len(city_nights) > 0
+    assert list(city_nights[1]) == ['citynights__music.wav']
+
+def test_create_scripts():
+    create_script= Script().create(scriptText="<<sectionName::question>> Hey! Do you know we support multiple voices from different providers in the same script? I am a polly voice from Amazon. <<sectionName::answer>> I am Azure voice from Microsoft. I think Azure voices sound awesome.")
+    scriptId = create_script.get("scriptId")
+    create_speech = Speech().create(scriptId=scriptId)
+    create_tts = {'message': 'Success. Text-to-speech is in progress.'}
+    assert create_speech == create_tts
+
+def test_create_mastering():
+    create_script= Script().create(scriptText="<<sectionName::question>> Hey! Do you know we support multiple voices from different providers in the same script? I am a polly voice from Amazon. <<sectionName::answer>> I am Azure voice from Microsoft. I think Azure voices sound awesome.")
+    scriptId = create_script.get("scriptId")
+    Speech().create(scriptId=scriptId)
+    mastering = Mastering().create(scriptId=scriptId)
+    create_mastering = {'Message': 'Mastering request was successful'}
+    assert len(mastering) > 0
+    assert mastering == create_mastering
