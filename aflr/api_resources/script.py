083549f31750b77f9b54310ae73013ef2f3f86aa
a96lex <a96lex@gmail.com>
2021-05-27 13:21:47 +0200

    add /script/random call

--- aflr/api_resources/script.py
+++ aflr/api_resources/script.py
@@ -4,3 +4,10 @@ from aflr.helper_classes import CreatableResource, ListableResource, Retrievable
 class Script(ListableResource, CreatableResource, RetrievableResource):
     OBJECT_NAME = "script"
     resource_path = "/script"
+    random_url = "/script/random"
+
+    @classmethod
+    def create_random(cls, randomText=None):
+        return cls._get_request(
+            path_param=cls.random_url, request_params={"randomText": randomText}
+        )
