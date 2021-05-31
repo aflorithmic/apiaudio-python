deecec014003db24977a4773c88812883607cbb4
FeaR6077 <julian@aflorithmic.ai>
2021-05-31 16:39:44 +0200

    added func. to upload/query and download files

--- aflr/api_resources/__init__.py
+++ aflr/api_resources/__init__.py
@@ -3,3 +3,4 @@ from aflr.api_resources.speech import Speech
 from aflr.api_resources.voice import Voice
 from aflr.api_resources.mastering import Mastering
 from aflr.api_resources.sound import Sound
+from aflr.api_resources.file import File
--- aflr/api_resources/file.py
+++ aflr/api_resources/file.py
@@ -1,19 +1,62 @@
+from posixpath import basename
 import aflr
-from aflr.api_request import APIRequest
+from aflr.helper_classes import ListableResource
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import os
+import requests
+import json
 
 
-class File(APIRequest):
+class File(ListableResource):
     OBJECT_NAME = "file"
+    resource_path = "/file"
+    custom_audio_upload = "/file/customaudio/uploadurl?filename="
+    by_tag = "/file/customaudio/tags?tags="
+    by_metaId = "/file/customaudio/mediaid?mediaId="
+    by_org = "/file/customaudio/org"
 
-    def __init__(self):
-        super().__init__()
-        self.url = self.api_base + "/file"
+    @classmethod
+    def upload_audiofile(cls):
+        # get presigned URL
+        payload = askopenfilename()
+        filename = os.path.basename(payload)
+        headers = {"Content-Type": "audio/mpeg"}
+        print("URL", cls.custom_audio_upload)
+        url = cls._get_request(path_param=cls.custom_audio_upload + filename)
 
-    def config_test(self):
-        return f"Configured to transact {self.OBJECT_NAME} objects to {self.url} with api_key = {self.api_key}"
+        mediaId = url["mediaId"]
+        fileUploadUrl = url["fileUploadUrl"]
+        print("mediaId", mediaId)
 
-    # get speech files
-    # TBD
+        # Do put request with presigned URL
+        response = requests.request(
+            "PUT", url=fileUploadUrl, headers=headers, data=payload
+        )
 
-    # get sound files
-    # TBD
+        response = {
+            "message": f" Success. Please make sure to save this mediaId : {mediaId}"
+        }
+        return response
+
+    @classmethod
+    def download_audiofile(cls, metaId):
+        url = cls._get_request(path_param=cls.by_metaId + metaId)
+        return url
+
+    @classmethod
+    def search_audiofiles(cls):
+        response = cls._get_request(path_param=cls.by_org)
+        return response
+
+    @classmethod
+    def search_audiofiles_by_tag(cls, tags):
+        url = cls._get_request(path_param=cls.by_tag + tags)
+        return url
+
+
+# get speech files
+# TBD
+
+# get sound files
+# TBD
