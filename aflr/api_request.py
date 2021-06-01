1e78d8919a58753cc4838361c9ca38da31558dd2
a96lex <a96lex@gmail.com>
2021-06-01 11:32:18 +0200

    newline convention

--- aflr/api_request.py
+++ aflr/api_request.py
@@ -10,8 +10,10 @@ class APIRequest:
     def _api_key_checker(api_key=None):
         if api_key == None or api_key == "your-key":
             api_key = os.environ.get("aflr_key", None)
+
         if not isinstance(api_key, str):
             raise TypeError("api_key must be of type string.")
+
         if len(api_key) < 32:
             raise ValueError(
                 "Please specify a valid api_key or create one here:\n https://console.api.audio"
@@ -20,15 +22,19 @@ class APIRequest:
     @classmethod
     def _build_header(cls):
         cls._api_key_checker(aflr.api_key)
-        # add more headers for analytics in the future
+
         return {"x-api-key": aflr.api_key}
 
     @classmethod
     def _post_request(cls, json, url=None):
         url = url or f"{aflr.api_base}{cls.resource_path}"
+
         headers = cls._build_header()
+
         r = requests.post(url=url, headers=headers, json=json)
+
         cls._expanded_raise_for_status(r)
+
         return r.json()
 
     @classmethod
@@ -44,17 +50,22 @@ class APIRequest:
             r = requests.get(url=url, headers=headers, params=request_params)
         else:
             r = requests.get(url=url, headers=headers)
+
         cls._expanded_raise_for_status(r)
+
         return r.json()
 
     @classmethod
     def _download_request(cls, url, destination):
         local_filename = f"{destination}/{url.split('/')[-1].split('?')[0]}"
         local_filename = local_filename.replace("%243ct10n", "section")
+
         with requests.get(url, stream=True) as r:
             cls._expanded_raise_for_status(r)
+
             with open(local_filename, "wb") as f:
                 shutil.copyfileobj(r.raw, f)
+
         return local_filename
 
     @classmethod
