c116cf8c7c986c0b3128501c20e18b8b8b43bfa8
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2021-07-14 18:44:19 +0200

    upgrade to version v0.8.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="apiaudio",
-    version="0.8.2",
+    version="0.8.3",
     author="Antonio Tripiana",
     author_email="antonio@aflorithmic.ai",
     description="Python SDK for api.audio API",
