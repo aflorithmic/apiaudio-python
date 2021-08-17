c2f126d60d2ac2fecbb9922a12df84f1f3b95539
a96lex <a96lex@gmail.com>
2021-08-17 12:59:20 +0200

    improved media resource

--- apiaudio/api_resources/media.py
+++ apiaudio/api_resources/media.py
@@ -2,30 +2,37 @@ from apiaudio.helper_classes import UploadableResource
 
 
 class Media(UploadableResource):
-    audio_resource_path = "/file/customaudio/"
+    audio_resource_path = "/file/customaudio"
     OBJECT_NAME = "file"
     resource_path = "/file"
 
     @classmethod
     def get_download_url(cls, mediaId):
         return cls._get_request(
-            path_param=cls.audio_resource_path + "mediaid?",
+            path_param=cls.audio_resource_path,
             request_params={"mediaId": mediaId},
         )
 
     @classmethod
-    def list(cls, tags=None, mediaId=None):
+    def list(cls, tags: str = "", mediaId: str = "", downloadUrl: bool = False) -> dict:
+        request_params = {}
+
+        if downloadUrl:
+            request_params["target"] = "download"
+
         if mediaId:
             return cls._get_request(
                 path_param=cls.audio_resource_path,
-                request_params={"mediaId": mediaId},
+                request_params={**request_params, **{"mediaId": mediaId}},
             )
         if tags:
             return cls._get_request(
                 path_param=cls.audio_resource_path,
-                request_params={"tags": tags},
+                request_params={**request_params, **{"tags": tags}},
             )
-        return cls._get_request(path_param=cls.audio_resource_path)
+        return cls._get_request(
+            path_param=cls.audio_resource_path, request_params=request_params
+        )
 
     @classmethod
     def list_tags(cls):
