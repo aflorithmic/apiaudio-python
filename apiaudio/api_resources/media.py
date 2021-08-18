3ba04b9a6b8eb7cb5a3cec5d2fbc3dee78cc939f
a96lex <a96lex@gmail.com>
2021-08-18 12:08:49 +0200

    ran black

--- apiaudio/api_resources/media.py
+++ apiaudio/api_resources/media.py
@@ -39,12 +39,16 @@ class Media(UploadableResource):
         return cls._get_request(path_param=f"{cls.audio_resource_path}/tags")
 
     @classmethod
-    def download(cls,mediaId: str="",destination:str="."):
+    def download(cls, mediaId: str = "", destination: str = "."):
         if not mediaId:
             raise ValueError("please specify a mediaId")
 
-        url = cls.list(mediaId=mediaId,downloadUrl=True).get("savedAudioFiles",[{}])[0].get("downloadUrl",None)
+        url = (
+            cls.list(mediaId=mediaId, downloadUrl=True)
+            .get("savedAudioFiles", [{}])[0]
+            .get("downloadUrl", None)
+        )
         if not url:
             raise ValueError("url could not be retrieved")
-        
-        return cls._download_request(url=url,destination=destination)
+
+        return cls._download_request(url=url, destination=destination)
