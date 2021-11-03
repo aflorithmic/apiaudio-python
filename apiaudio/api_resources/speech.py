544a168de810da6725a705fa94c1ce963c05e31b
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2021-11-03 09:54:11 +0100

    add speech sync to SyncTTS class

--- apiaudio/api_resources/speech.py
+++ apiaudio/api_resources/speech.py
@@ -8,8 +8,3 @@ from apiaudio.helper_classes import (
 class Speech(CreatableResource, RetrievableResource, DownloadableResource):
     OBJECT_NAME = "speech"
     resource_path = "/speech"
-    sync_tts_path = "/speech/sync"
-
-    @classmethod
-    def tts(cls, **params):
-        return cls._post_request_raw(json=params, url=f"{cls.sync_tts_path}", istype="wav")
