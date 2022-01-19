9462e612904a0595475322cc803eb86bd8079718
Sjhunt93 <Sjhunt93@gmail.com>
2022-01-19 12:02:18 +0000

    Cleanup

--- apiaudio/api_resources/script.py
+++ apiaudio/api_resources/script.py
@@ -11,19 +11,6 @@ class Script(ListableResource, CreatableResource, RetrievableResource, Deletable
     resource_path = "/script"
     random_url = "/script/random"
 
-    # @classmethod
-    # def create(cls, **params):
-    #     if 'scriptId' in params:
-    #         try:
-    #             response = Script.retrieve(scriptId=params['scriptId'])
-    #             print("Warning - This scriptId already exists. You should use update(). This will be deprecated in future versions of the API")
-    #             # -> re-direct to update
-    #             return super(Script, cls).create(**params)
-
-    #         except Exception as e:
-    #             return super(Script, cls).create(**params)
-        
-    #     #return cls._post_request(json=params)
 
     @classmethod
     def get_random_text(cls, category=None):
