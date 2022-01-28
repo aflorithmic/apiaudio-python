40be36891e1aad2805b19fd5b24feeb4bfda709d
Marcin Martinez <fishermann@Marcins-Air.home>
2022-01-28 15:46:57 +0100

    fallback to empty list

--- apiaudio/api_request.py
+++ apiaudio/api_request.py
@@ -144,7 +144,7 @@ class APIRequest:
         """
         try:
             if res.headers.get("Warning"):
-                for warn in res.json().get("warnings"):
+                for warn in res.json().get("warnings", []):
                     SDKLogger(self.OBJECT_NAME).warning(warn)
             
             latest = res.headers.get("x-latest-python", sdk_version)
