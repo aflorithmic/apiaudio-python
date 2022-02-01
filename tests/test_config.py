f9637d141d33bf1ec70ba6d537557cde3f7d3d02
Marcin Martinez <fishermann@Marcins-Air.home>
2022-02-01 19:26:12 +0100

    update tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,41 +3,28 @@ import pprint
 import pytest
 import os 
 
-apiaudio.api_key = os.getenv("AFLR_API_KEY_PEADAR")
-def test_english_voices():
-    print("List test:\nAll voices:")
-    print(len(apiaudio.Voice.list().get("voices")))
-    print("\nFiltered voices:")
-    assert len(apiaudio.Voice.list(language="english").get("voices")) == 16
-
-def test_spanish_voices():
-    assert len(apiaudio.Voice.list(language="spanish").get("voices")) == 7
+apiaudio.api_key = os.environ["AFLR_API_KEY"]
+#apiaudio.api_base="https://staging-v1.api.audio"
 
 def test_list_parameters():
     assert len(apiaudio.Voice.list_parameters()) > 0 
 
 def test_sound_design():
-    sounds = apiaudio.Sound.list_sound_templates()
-    city_nights=sounds.get("templateFiles").get("citynights")
-    # Still not sure how to get this to work properly
-    assert len(city_nights) > 0
-    assert list(city_nights[1]) == ['citynights__music.wav']
+    sounds = apiaudio.Sound.list()
+    print(sounds)
 
 def test_create_scripts():
-    create_script= apiaudio.Script.create(scriptText="<<sectionName::question>> Hey! Do you know we support multiple voices from different providers in the same script? I am a polly voice from Amazon. <<sectionName::answer>> I am Azure voice from Microsoft. I think Azure voices sound awesome.")
-    scriptId = create_script.get("scriptId")
-    create_speech = apiaudio.Speech.create(scriptId=scriptId)
-    create_tts = {'message': 'Success. Text-to-speech is in progress.'}
-    assert create_speech == create_tts
+    create_script= apiaudio.Script.create(scriptId="test_sdk", scriptText="<<sectionName::question>> Hey! Do you know we support multiple voices from different providers in the same script? I am a polly voice from Amazon. <<sectionName::answer>> I am Azure voice from Microsoft. I think Azure voices sound awesome.")
+    get_script = apiaudio.Script.retrieve(scriptId="test_sdk")
+    apiaudio.Speech.create(scriptId="test_sdk", voice="brandon")
 
 def test_create_mastering():
     create_script= apiaudio.Script.create(scriptText="<<sectionName::question>> Hey! Do you know we support multiple voices from different providers in the same script? I am a polly voice from Amazon. <<sectionName::answer>> I am Azure voice from Microsoft. I think Azure voices sound awesome.")
     scriptId = create_script.get("scriptId")
-    apiaudio.Speech.create(scriptId=scriptId)
+    apiaudio.Speech.create(scriptId=scriptId, voice="brandon")
     mastering = apiaudio.Mastering.create(scriptId=scriptId)
-    create_mastering = {'Message': 'Mastering request was successful'}
     assert len(mastering) > 0
-    assert mastering == create_mastering
+    assert len(mastering["url"]) > 0
 
 def test_birdcache():
     birdcache = apiaudio.Birdcache.create(
@@ -50,3 +37,19 @@ def test_birdcache():
     assert len(birdcache) > 0
     assert birdcache[1]["text"] == "This is salih creating synchronous text to speech"
 
+def test_script_versions():
+    from .assets import test_versions
+
+    script = apiaudio.Script.create(
+        scriptId="test_sdk",
+        scriptText="...",
+        versions=test_versions
+    )
+    
+    assert script.get("availableVersions") == ["v0"] + [str(x) for x in test_versions.keys()]
+
+    for ver in test_versions:
+        speech = apiaudio.Speech.create(scriptId=script.get("scriptId"), version=ver)
+        key = str(ver) + "|default"
+        assert key in speech.keys()
+        assert speech[key]["status_code"] == "201"
